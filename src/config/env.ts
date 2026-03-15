217f1d13af3f4f8e314e775a0ddde482e80be5e9
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-15 16:46:50 +0545

    feat(config): add Zod-validated env config

---
+++ src/config/env.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import z from 'zod'
+
+const envSchema = z.object({
+    PORT: z.string().min(1),
+    DATABASE_URL: z.string().url(),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    CLIENT_URL: z.string().min(1),
+    JWT_ACCESS_SECRET: z.string().min(1),
+    JWT_ACCESS_EXPIRES_IN: z.string().min(1),
+    JWT_REFRESH_SECRET: z.string().min(1),
+    JWT_REFRESH_EXPIRES_IN: z.string().min(1),
+})
+
+const _env = envSchema.safeParse(process.env);
+
+if(!_env.success){
+    console.error("❌ Invalid environment variables:", _env.error.format());
+    process.exit(1)
+}
+
+export const env = _env.data!;
