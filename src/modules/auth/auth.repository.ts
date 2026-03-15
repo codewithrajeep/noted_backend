a83e2c9fbcbb0e977c248ea6363f305ed6fd6fcc
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-15 22:24:49 +0545

    refactor(auth): exclude password from create query using select

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -5,6 +5,14 @@ export const AuthRepository = {
   create: async (data: Prisma.UserCreateInput) => {
     const user = await prisma.user.create({
       data,
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
     });
     return user;
   },
