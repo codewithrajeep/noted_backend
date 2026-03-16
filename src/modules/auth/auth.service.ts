f1c7c5708921e2ad7acd97ab4d05eff50304afcb
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-16 13:08:00 +0545

    feat(auth): complete login with JWT access and refresh token generation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { AppError } from "../../utils/AppError";
+import { generateAccessToken, generateRefreshToken } from "../../utils/token";
 import { AuthRepository } from "./auth.repository";
 import { LoginInput, RegisterInput } from "./auth.schema";
 import bcrypt from "bcrypt";
@@ -21,7 +22,13 @@ export const AuthService = {
     const isPasswordMatch = await bcrypt.compare(data.password, user.password!);
     if (!isPasswordMatch) throw new AppError("Invalid credentials", 401);
     const { password: _password, ...safeUser } = user;
-    // TODO: Generate JWT token
-    return safeUser;
+    const payload = {
+      id: safeUser.id,
+      email: safeUser.email,
+      username: safeUser.username,
+    };
+    const accessToken = generateAccessToken(payload);
+    const refreshToken = generateRefreshToken(payload);
+    return { user: safeUser, accessToken, refreshToken };
   },
 };
