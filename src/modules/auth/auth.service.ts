cdbd65db31c3820090328a9885e13a20ed63df6b
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-16 12:41:56 +0545

    feat(auth): add auth service with register and login business logic

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../utils/AppError";
 import { AuthRepository } from "./auth.repository";
 import { LoginInput, RegisterInput } from "./auth.schema";
 import bcrypt from "bcrypt";
@@ -5,7 +6,7 @@ import bcrypt from "bcrypt";
 export const AuthService = {
   register: async (data: RegisterInput) => {
     const existing = await AuthRepository.findByEmail(data.email);
-    if (existing) throw new Error("User already exists");
+    if (existing) throw new AppError("User already exists", 409);
     const hashedPassword = await bcrypt.hash(data.password, 10);
 
     const user = await AuthRepository.create({
@@ -16,9 +17,9 @@ export const AuthService = {
   },
   login: async (data: LoginInput) => {
     const user = await AuthRepository.findByEmail(data.email);
-    if (!user) throw new Error("User not found");
+    if (!user) throw new AppError("Invalid credentials", 401);
     const isPasswordMatch = await bcrypt.compare(data.password, user.password!);
-    if (!isPasswordMatch) throw new Error("Invalid credentials");
+    if (!isPasswordMatch) throw new AppError("Invalid credentials", 401);
     const { password: _password, ...safeUser } = user;
     // TODO: Generate JWT token
     return safeUser;
