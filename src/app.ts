b728fcef7abd2cff7c60985312a870670603e52e
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-16 16:33:23 +0545

    refactor(app): register global error handler after routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { env } from "./config/env";
 import routes from "./routes/index";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -17,5 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", routes);
+
+app.use(errorHandler);
 
 export default app;
