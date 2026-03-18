fdeb79095f40cfaf5f385cde01427df66b265f4d
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-18 15:23:56 +0545

    feat(auth): add refresh controller with missing token validation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AuthService } from "./auth.service";
 import { loginSchema, registerSchema } from "./auth.schema";
+import { AppError } from "../../utils/AppError";
 
 export const AuthController = {
   register: async (req: Request, res: Response, next: NextFunction) => {
@@ -24,6 +25,22 @@ export const AuthController = {
         success: true,
         message: "User logged in successfully",
         data: user,
+      });
+    } catch (err) {
+      next(err);
+    }
+  },
+  refresh: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { refreshToken } = req.body;
+      if (!refreshToken) {
+        return next(new AppError("Refresh token is required", 400));
+      }
+      const accessToken = await AuthService.refresh(refreshToken);
+      return res.status(200).json({
+        success: true,
+        message: "Access token generated successfully",
+        data: accessToken,
       });
     } catch (err) {
       next(err);
