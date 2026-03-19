6f464fe09fb3ed49a113674c85ab12ffd74fd152
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-19 22:33:45 +0545

    feat(task): add pagination and filtering to findAll repository

--- src/modules/task/task.repository.ts
+++ src/modules/task/task.repository.ts
@@ -1,5 +1,6 @@
 import { Prisma } from "../../generated/prisma";
 import prisma from "../../lib/prisma";
+import { TaskQueryInput } from "./task.schema";
 
 export const TaskRepository = {
   create: async (data: Prisma.TaskCreateInput) => {
@@ -8,13 +9,30 @@ export const TaskRepository = {
     });
     return task;
   },
-  findAll: async (userId: string) => {
-    const tasks = await prisma.task.findMany({
-      where: {
-        createdById: userId,
-      },
-    });
-    return tasks;
+  findAll: async (userId: string, query: TaskQueryInput) => {
+    const page = parseInt(query.page || "1");
+    const limit = parseInt(query.limit || "10");
+    const skip = (page - 1) * limit; // eg: (2page - 1) * 10limit = 10task
+    const [tasks, total] = await Promise.all([
+      prisma.task.findMany({
+        where: {
+          createdById: userId,
+          status: query.status || undefined,
+        },
+        orderBy: {
+          [query.sortBy || "createdAt"]: query.order || "desc",
+        },
+        skip,
+        take: limit,
+      }),
+      prisma.task.count({
+        where: {
+          createdById: userId,
+          status: query.status,
+        },
+      }),
+    ]);
+    return { tasks, total, page, limit };
   },
   findById: async (id: string) => {
     const task = await prisma.task.findUnique({
