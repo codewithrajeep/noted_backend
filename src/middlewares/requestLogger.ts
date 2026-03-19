654091f1d512594de33a2bdf6cf955f12f13e8c1
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-19 21:57:05 +0545

    feat(middleware): add request logger with method, url, status, and response time

---
+++ src/middlewares/requestLogger.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import logger from "../lib/logger";
+
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    const responseTime = Date.now() - start;
+    logger.info({
+      method: req.method,
+      url: req.url,
+      statusCode: res.statusCode,
+      responseTime: `${responseTime}ms`,
+      ip: req.ip,
+    });
+  });
+  next();
+};
