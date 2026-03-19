f2aa48ce880eef685e93fc3c77dc251a89db71ad
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-19 22:05:19 +0545

    feat(app): add helmet for HTTP security headers

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import { env } from "./config/env";
 import routes from "./routes/index";
 import { errorHandler } from "./middlewares/errorHandler";
 import { requestLogger } from "./middlewares/requestLogger";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet())
 app.use(express.json());
 app.use(
   cors({
