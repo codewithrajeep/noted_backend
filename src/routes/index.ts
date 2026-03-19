d514811209bb3e4149bbd7caa5111ff09c366aec
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-19 16:50:46 +0545

    feat(routes): mount task routes under /api/task

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import userRoutes from "../modules/user/user.routes";
+import taskRoutes from "../modules/task/task.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-router.use("/user", userRoutes)
+router.use("/user", userRoutes);
+router.use("/task", taskRoutes);
 
 export default router;
